5238d7af5f3a8504c8df536be906d2afbeb95fe8
lordvidex <lordvidex.prime@gmail.com>
2020-12-24 05:04:18 +0300

    Refetched files.. saving backup for autotest experimentations

---
+++ AutoTest.java
@@ -0,0 +1,64 @@
+import java.io.*;
+import java.util.*;
+
+public class AutoTest{
+    public static File[] getAllFiles(String path){
+        File parentFile = new File(path);
+        if(!parentFile.isDirectory()){
+            System.out.println("Not a directory");
+            System.exit(1);
+        }
+        FileFilter ff = new FileFilter(){
+            @Override
+            public boolean accept(File file){
+                if(file.getName().startsWith(".")||!file.isDirectory())return false;
+                return true;
+            }
+        };
+        return parentFile.listFiles(ff);
+    }
+    public static void main(String[] args) throws InterruptedException,IOException{
+        String src = (args.length==0)?".":args[0];
+        File[] files = getAllFiles(src);
+
+        for(File f: files){
+            File testFile = new File(f.getAbsolutePath()+"/test/"+f.getName().replaceAll("-","_")+"_test.dart");
+            unSkipAllTests(testFile);
+        }
+
+        // for(File f: files){
+        //     Process proc = Runtime.getRuntime()
+        //     .exec("pub get", null, new File(f.getAbsolutePath()));
+        //     printResult(proc);
+        //     Process proc2 = Runtime.getRuntime().exec("pub run test",null,new File(f.getAbsolutePath()));
+        //     printResult(proc2);
+        // }
+        
+    }
+    public static void unSkipAllTests(File file) throws IOException {
+        ArrayList<String> store = new ArrayList<>();
+        BufferedReader br = new BufferedReader(new FileInputStream(file));
+        BufferedWriter bw = new BufferedWriter(new FileOutputStream(file));
+        String line;
+        while((line = br.readLine())!=null){
+            if(line.contains("skip: true")){
+                line = line.replace("skip: true", "skip: false");
+            }
+            store.add(line);
+        }
+        for(String val: store){
+            bw.write(val+"\n");
+        }
+        br.close();
+        bw.close();
+    }
+    public static void printResult(Process proc)throws IOException,InterruptedException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(proc.getInputStream()));
+        String line ;
+        while((line = br.readLine())!=null){
+            System.out.println(line);
+        }
+        proc.waitFor();
+        proc.destroy();
+    }
+}
--- nth-prime/lib/nth_prime.dart
+++ nth-prime/lib/nth_prime.dart
@@ -1,30 +1,35 @@
 class NthPrime {
-  static int MAX_SIZE = 150000;//since test does not exceed this
+  static int MAX_SIZE = 150000; //since test does not exceed this
   bool done = false;
   List<int> primes = [];
-  void SieveOfErathoStenes(){
+  void SieveOfErathoStenes() {
     List<bool> isPrime = [];
-    for(int i = 0; i<MAX_SIZE;i++){
+    for (int i = 0; i < MAX_SIZE; i++) {
       isPrime.add(true);
     }
-    for(int i = 2; i*i<MAX_SIZE;i++){
-      if(isPrime[i]){
-        for(int p = i*i; p<MAX_SIZE; p+=i){
+    for (int i = 2; i * i < MAX_SIZE; i++) {
+      if (isPrime[i]) {
+        for (int p = i * i; p < MAX_SIZE; p += i) {
           isPrime[p] = false;
         }
       }
     }
-    for(int i =2;i<MAX_SIZE;i++){
-      if(isPrime[i]){
+    for (int i = 2; i < MAX_SIZE; i++) {
+      if (isPrime[i]) {
         primes.add(i);
       }
     }
   }
-  int prime(int pos){
-    if(!done){
+
+  int prime(int pos) {
+    if (!done) {
       SieveOfErathoStenes();
       done = true;
     }
-    return primes[pos-1];
+    try {
+      return primes[pos - 1];
+    } catch (e) {
+      throw new ArgumentError('There is no zeroth prime');
+    }
   }
 }
